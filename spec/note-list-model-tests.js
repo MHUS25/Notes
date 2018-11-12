4c0b847b296cc528530e8fd57302bb9ae00f5add
Muna <mmhussein125@gmail.com>
2018-11-12 16:06:39 +0000

    adding new notes feature

--- spec/note-list-model-tests.js
+++ spec/note-list-model-tests.js
@@ -5,3 +5,13 @@ function testNoteListStartWithOneEntry() {
 };
 
 testNoteListStartWithOneEntry();
+
+
+function testAddingNewNotes() {
+  var noteList = new NoteList();
+  noteList.addNote('Hello');
+  assert.isTrue(noteList.text === 'Hello');
+  assert.isTrue(noteList.getNotes().slice(-1)[0] === 'Hello');
+};
+
+testAddingNewNotes();
--- src/note-list-model.js
+++ src/note-list-model.js
@@ -1,7 +1,18 @@
 (function(exports) {
   function NoteList() {
     this.entries = ['My favourite language is JavaScript'];
+    this.text = 'Favourite drink: seltzer';
+
   };
 
   exports.NoteList = NoteList;
 })(this);
+
+NoteList.prototype.getNotes = function() {
+  return this.entries;
+};
+
+NoteList.prototype.addNote = function(note) {
+  this.text = note;
+  this.entries.push(note);
+};
