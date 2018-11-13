15c47ed9ed7a9c4521df1f46cea94c2d7f90d781
Muna <mmhussein125@gmail.com>
2018-11-13 17:19:44 +0000

    step6 incomplete

---
+++ spec/note-controller-tests.js
@@ -0,0 +1,10 @@
+function noteControllerCanBeInstantiated() {
+  var noteController= new NoteController
+    assert.isInstanceOf(noteController, NoteController);
+}
+noteControllerCanBeInstantiated()
+
+function innerHTMLPropertyChanges() {
+  var noteController= new NoteController
+  
+}
--- src/note-controller.js
+++ src/note-controller.js
@@ -1,2 +1,19 @@
+(function(exports) {
+  function NoteController() {
+    this.noteList = new NoteList
+  }
 
-document.getElementById('app').textContent="howdy";
+  NoteController.prototype.addsNote = function() {
+    this.noteList.addNote('Favourite drink: Fanta');
+  }
+
+  NoteController.prototype.insertHTML = function () {
+    var noteListView = new NoteListView(this.notelist);
+    var html = noteListView.noteListHTML();
+    document.getElementById('app').textContent="howdy";
+  };
+
+  // document.getElementById('app').textContent="howdy";
+
+  exports.NoteController = NoteController
+})(this)
