e53e12231e58f40460eea641ca2cb3775700ac02
Muna <mmhussein125@gmail.com>
2018-11-14 14:01:01 +0000

    Only append ellipses to notes with more than 20 chars

--- spec/note-list-view-tests.js
+++ spec/note-list-view-tests.js
@@ -8,8 +8,9 @@ testNoteListViewStartsEmpty()
 function testNoteListViewWithNotesAdded() {
   var noteList = new NoteList
   var noteListView = new NoteListView(noteList)
+  noteList.addNote('Hello World!')
   noteList.addNote('Coding is so much fuuuuuuuun')
   noteList.addNote('I lo0o0o0o0o0o0o0ove JavaScript')
-  assert.isEqualTo(noteListView.noteListHTML(), '<ul><li><div>Coding is so much fu...</div></li><li><div>I lo0o0o0o0o0o0o0ove...</div></li></ul>')
+  assert.isEqualTo(noteListView.noteListHTML(), '<ul><li><div>Hello World!</div></li><li><div>Coding is so much fu...</div></li><li><div>I lo0o0o0o0o0o0o0ove...</div></li></ul>')
 }
 testNoteListViewWithNotesAdded()
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,11 +1,19 @@
 (function(exports) {
   function NoteListView(noteList) {
     this.noteList = noteList
+    MAX_LENGTH = 20
+  }
+
+  NoteListView.prototype._abbreviation = function(string) {
+    if (string.length > MAX_LENGTH) {
+      return string.slice(0,MAX_LENGTH).concat('...')
+    }
+    return string
   }
 
   NoteListView.prototype.noteListHTML = function() {
     list = this.noteList.getNotes().map(
-      note => `<li><div>${note.getText().slice(0,20).concat('...')}</div></li>`
+      note => `<li><div>${this._abbreviation(note.getText())}</div></li>`
     ).join('')
     return `<ul>${list}</ul>`
   }
