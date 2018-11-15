0ea5b542865e3a007f04091a20b8d201a8880b17
Muna <mmhussein125@gmail.com>
2018-11-15 15:59:51 +0000

    Finishing the implementation of a single note page

--- spec/note-controller-tests.js
+++ spec/note-controller-tests.js
@@ -10,6 +10,7 @@ function testInnerHTMLPropertyCanBeChanged() {
   div.id = 'app';
   document.body.appendChild(div);
   noteController.insertHTML();
+  console.log(document.getElementById('app'));
   assert.isEqualTo(div.innerHTML, '<ul><li><div><a href="#notes/0">Favourite drink: Fan...</a></div></li></ul>');
 }
 testInnerHTMLPropertyCanBeChanged()
--- src/note-controller.js
+++ src/note-controller.js
@@ -10,5 +10,32 @@
     document.getElementById('app').innerHTML = html;
   };
 
+  NoteController.prototype.makeUrlChangeShowNoteForCurrentPage = function() {
+    bounded = this._showNoteForCurrentPage.bind(this)
+    window.addEventListener("hashchange", bounded);
+  };
+
+  NoteController.prototype._showNoteForCurrentPage = function() {
+    var noteId = this._getNoteIdFromUrl(window.location);
+    var note = this._findNoteById(noteId)
+    this._showNote(note);
+  };
+
+  NoteController.prototype._getNoteIdFromUrl = function(location) {
+    return location.hash.split("#notes/")[1];
+  }
+
+  NoteController.prototype._showNote = function(note) {
+    var singleNoteView = new SingleNoteView(note)
+    var div = document.getElementById('app');
+    div.innerHTML = singleNoteView.viewNote();
+  }
+
+  NoteController.prototype._findNoteById = function(id) {
+    return this.noteList.getNotes().find(function(note) {
+      return note.id = id;
+    })
+  };
+
   exports.NoteController = NoteController
 })(this)
